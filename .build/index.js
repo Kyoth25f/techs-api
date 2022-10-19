d66cbaf49d2d0f2acefcc1c824a9a4910e9b3a6f
Vitor <73979580+Blackklegend@users.noreply.github.com>
2022-10-19 17:16:03 +0000

    Initial commit

---
+++ .build/index.js
@@ -0,0 +1,29 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __reExport = (target, module2, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && key !== "default")
+        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toModule = (module2) => {
+  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", module2 && module2.__esModule && "default" in module2 ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
+};
+var import_express = __toModule(require("express"));
+var import_cors = __toModule(require("cors"));
+const app = (0, import_express.default)();
+const router = import_express.default.Router();
+router.get("/", function(req, res) {
+  res.send("All systems operational");
+});
+app.use((0, import_cors.default)());
+app.use(router);
+app.listen(3e3, () => console.log("Listening on port 3000"));
+//# sourceMappingURL=index.js.map
